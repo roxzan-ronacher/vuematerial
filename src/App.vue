e595e64672dd44e7d50a04e98d0ea2f36ca88959
Roxzan <roxzan@shuntyard.com>
2018-06-21 13:46:36 +0200

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: 'app',
+  data: function () {
+    return {
+      contacts: []
+    }
+  }
+}
+</script>
+
+<template>
+  <div id="app">
+  <md-button class="md-raised md-primary">Hi</md-button>
+  </div>
+</template>
